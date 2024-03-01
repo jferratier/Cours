d45f7d99fdcc29ced738d9ae50b0ad539907fd89
jerome ferratier <jerome.ferratier@spie.com>
2024-03-01 09:07:13 +0100

    Mise à jour SIO

---
+++ SIO/API .Net/WebApplication2/WebApplication2/Controllers/UserController.cs
@@ -0,0 +1,134 @@
+﻿using WebApplication2.Entities;
+using WebApplication2.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DemoContext DemoContext;
+
+        public UserController(DemoContext DemoContext)
+        {
+            this.DemoContext = DemoContext;
+        }
+
+        /// <summary>
+        /// Definition du Web Service
+        /// </summary>
+        /// <remarks>Je manque d'imagination</remarks>
+        /// <param name="id">id du client a retourné</param>   
+        /// <response code="200">client selectionné</response>
+        /// <response code="404">client introuvable pour l'id specifié</response>
+        /// <response code="500">Oops! le service est indisponible pour le moment</response>
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            var List = await DemoContext.Users.Select(
+                s => new User
+                {
+                    Id = s.Id,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Username = s.Username,
+                    Password = s.Password,
+                    EnrollmentDate = s.EnrollmentDate
+                }
+            ).ToListAsync();
+
+            if (List.Count < 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return List;
+            }
+        }
+
+        /// <summary>
+        /// Definition du Web Service totototoGetUserByName
+        /// </summary>
+        /// <remarks>bla bla </remarks>
+        /// <param name="id">Nom du client a retourner</param>   
+        /// <response code="200">client selectionné</response>
+        /// <response code="404">client introuvable pour l'id specifié</response>
+        /// <response code="500">Oops! le service est indisponible pour le moment</response>
+        [HttpGet("totototoGetUserByName")]
+        public async Task<ActionResult<User>> GetUserById(string FirstName)
+        {
+            User User = await DemoContext.Users.Select(
+                    s => new User
+                    {
+                        Id = s.Id,
+                        FirstName = s.FirstName,
+                        LastName = s.LastName,
+                        Username = s.Username,
+                        Password = s.Password,
+                        EnrollmentDate = s.EnrollmentDate
+                    })
+                .FirstOrDefaultAsync(s => s.FirstName == FirstName);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return User;
+            }
+        }
+
+        [HttpPost("InsertUser")]
+        public async Task<HttpStatusCode> InsertUser(User User)
+        {
+            var entity = new User()
+            {
+                Id = User.Id,
+                FirstName = User.FirstName,
+                LastName = User.LastName,
+                Username = User.Username,
+                Password = User.Password,
+                EnrollmentDate = User.EnrollmentDate
+            };
+
+            DemoContext.Users.Add(entity);
+            await DemoContext.SaveChangesAsync();
+
+            return HttpStatusCode.Created;
+        }
+
+        [HttpPut("UpdateUser")]
+        public async Task<HttpStatusCode> UpdateUser(User User)
+        {
+            var entity = await DemoContext.Users.FirstOrDefaultAsync(s => s.Id == User.Id);
+
+            entity.FirstName = User.FirstName;
+            entity.LastName = User.LastName;
+            entity.Username = User.Username;
+            entity.Password = User.Password;
+            entity.EnrollmentDate = User.EnrollmentDate;
+
+            await DemoContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+
+        [HttpDelete("DeleteUser/{Id}")]
+        public async Task<HttpStatusCode> DeleteUser(int Id)
+        {
+            var entity = new User()
+            {
+                Id = Id
+            };
+            DemoContext.Users.Attach(entity);
+            DemoContext.Users.Remove(entity);
+            await DemoContext.SaveChangesAsync();
+            return HttpStatusCode.OK;
+        }
+    }
+}
+
