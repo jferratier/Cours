4b47b2307eab90b2f864f481246e2d6f19f48268
Jerome Ferratier <jerome.ferratier@spie.com>
2025-09-19 11:19:29 +0200

    MAJ Correction TP

--- MVC2 TEST/Config/database.php
+++ MVC2 TEST/Config/database.php
@@ -3,6 +3,6 @@ define("DB_DRIVER", "mysql");
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_DATABASE","mvcsample");
+define("DB_DATABASE","mvc");
 define("DB_CHARSET", "utf8");
 ?>
--- MVC2 TEST/index.php
+++ MVC2 TEST/index.php
@@ -1,5 +1,42 @@
 <?php
 
-echo 1;
+require_once 'config/global.php';
+
+//Gestion du controller par defaut
+if(isset($_GET["controller"])){
+    $controllerObj = loadController($_GET["controller"]);
+    loadAction($controllerObj);
+    
+}else{
+    $controllerObj=loadController(CONTROLLER_DEFAULT);
+    loadAction($controllerObj);  
+}
+
+function loadController($controller){
+    switch($controller){
+        case 'articles' :
+            $strFileController = 'controller/articleController.php';
+            require_once $strFileController;
+            $controllerObj = new ArticlesController();
+        break;
+
+        default :
+            $strFileController = 'controller/articleController.php';
+            require_once $strFileController;
+            $controllerObj = new ArticlesController();
+        break;
+    }
+    return $controllerObj;
+}
+
+function loadAction($controllerObj){
+    if(isset($_GET["action"])){
+        $controllerObj->run($_GET["action"]);
+    }else{
+        $controllerObj->run(ACTION_DEFAULT);
+    }
+}
+
+
 
 ?>
