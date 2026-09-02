a402bb1f1d7f0a7670ef05202ac6541695488be6
jferratier <52955650+jferratier@users.noreply.github.com>
2026-09-02 16:50:59 +0200

    Delete SIO directory

--- SIO/MVC/MVC Advanced 2/view/composantsView/panelPresentation.php
+++
@@ -1,6 +0,0 @@
-<div class="container">
-    <div class="panel panel-primary">
-      <div class="panel-heading">Presentation de l'application</div>
-      <div class="panel-body">Cette application permet de gérer des articles</div>
-    </div>
-</div>
--- SIO/MVC/MVC Correction/config/global.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("CONTROLLER_DEFAULT", "articles");
-define("ACTION_DEFAULT","index");
-?>
